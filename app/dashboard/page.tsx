5d44ffebef4c61cbc6419b56582f5a1e2d1d504e
Jackson <armnellgomez@gmail.com>
2025-06-04 15:44:02 -0500

    Configure las variables de entorno de Supabase para build en producción en 'nextjs.yml'

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
   const handleLogout = async () => {
     const supabase = createClient();
     await supabase.auth.signOut();
-    router.push("/login");
+    router.push("/");
   };
 
   return (
