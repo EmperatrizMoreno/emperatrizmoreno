96438b02907e3f50e7000b3041d56c2a320c124b
Jackson <armnellgomez@gmail.com>
2025-06-20 17:32:24 -0500

    El dashboard mustra quien ha iniciado sesion

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const router = useRouter();
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const supabase = createClient();
+      const { data: { session } } = await supabase.auth.getSession();
+      const user = session?.user;
+      if (user) {
+        setNombre(user.user_metadata.nombre || "");
+        setApellido(user.user_metadata.apellido || "");
+      }
+    };
+    fetchUser();
+  }, []);
+
 
   const handleLogout = async () => {
     const supabase = createClient();
@@ -13,7 +30,7 @@ const Dashboard = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-2">Bienvenido al Dashboard</h1>
+      <h3 className="text-2xl font-bold mb-2">Hola {nombre} {apellido}</h3>
       <p className="mb-6">Solo puedes ver esto si has iniciado sesión.</p>
       <button
         onClick={handleLogout}
